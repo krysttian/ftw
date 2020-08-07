25d1731cbbc9d30d75fac532079ae673c95a1d36
krysttian <krysttian.duncan@gmail.com>
2020-08-07 12:31:40 -0400

    function to generate twilio client, use account SID per twilio

--- lib/functions/twilio.ts
+++ lib/functions/twilio.ts
@@ -1,37 +1,36 @@
-
-
 import * as twilio from 'twilio';
 
-export function sendEnrollmentConfirmation (phoneNumber: string, dlNumber: string) {
-    const truncatedDlNumber = dlNumber.slice(10);
-        const message = `You have been subscribed to SMS notifactions for the Driver license number ending in ${truncatedDlNumber}, \nReply STOP to unsubscribe. Msg&Data rates may apply.`
-      return sendSms(phoneNumber, message)
-}
-
-export function sendReportSMS (phoneNumber: string, dlNumber: string, report: string, source: string) {
+export function sendEnrollmentConfirmation(phoneNumber: string, dlNumber: string) {
   const truncatedDlNumber = dlNumber.slice(10);
-      const message = 
-      `This is a report using ${source} as the source for Driver License ending in ${truncatedDlNumber}. Reply STOP to unsubscribe. Msg&Data rates may apply. email support@drivefine.com if there is an issue with your report.\n
+  const message = `You have been subscribed to SMS notifactions for the Driver license number ending in ${truncatedDlNumber}, \nReply STOP to unsubscribe. Msg&Data rates may apply.`
+  return sendSms(phoneNumber, message)
+}
+
+export function sendReportSMS(phoneNumber: string, dlNumber: string, report: string, source: string) {
+  const truncatedDlNumber = dlNumber.slice(10);
+  const message =
+    `This is a report using ${source} as the source for Driver License ending in ${truncatedDlNumber}. Reply STOP to unsubscribe. Msg&Data rates may apply. email support@drivefine.com if there is an issue with your report.\n
       ${report}`
-    return sendSms(phoneNumber, message)
+  return sendSms(phoneNumber, message)
 }
 
-function sendSms (phoneNumber: string, message: string) {
-    console.log('sending sms');
-    const TWILIO_CLIENT_ID = process.env['TWILIO_CLIENT_ID'];
-    const TWILIO_AUTH_KEY = process.env['TWILIO_AUTH_KEY'];
-    const twilioClient = twilio(TWILIO_CLIENT_ID, TWILIO_AUTH_KEY);
-    return Promise.all([twilioClient.messages.create({
-        body: message,
-        to: phoneNumber, // Text this number
-        from: '+17866289828' // From a valid Twilio number
-      })])
+const generateTwilioClient = () => twilio(process.env['TWIIO_ACCOUNT_SID'], process.env['TWILIO_AUTH_KEY']);
+
+function sendSms(phoneNumber: string, message: string) {
+  console.log('sending sms');
+  const twilioClient = generateTwilioClient();
+  return Promise.all([twilioClient.messages.create({
+    body: message,
+    to: phoneNumber, // Text this number
+    from: '+17866289828' // From a valid Twilio number
+  })])
 }
 
-export function lookupPhoneNumber (phoneNumber: string) {
-  const TWILIO_CLIENT_ID = process.env['TWILIO_CLIENT_ID'];
-  const TWILIO_AUTH_KEY = process.env['TWILIO_AUTH_KEY'];
-  const twilioClient = twilio(TWILIO_CLIENT_ID, TWILIO_AUTH_KEY);
+export function lookupPhoneNumber(phoneNumber: string) {
+  console.log('looking up number');
+  const twilioClient = generateTwilioClient();
   return twilioClient.lookups.phoneNumbers(phoneNumber)
-  .fetch({countryCode: 'US'})
+    .fetch({
+      countryCode: 'US'
+    })
 }
